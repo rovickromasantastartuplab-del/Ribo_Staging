57b68647c6e186c11630afe0ff89e6c06a0e7ff9
Rovick <rovickromasanta.startuplab@gmail.com>
2026-02-19 21:56:13 +0800

    Update wedding suppliers index

--- resources/js/pages/wedding-suppliers/index.tsx
+++ resources/js/pages/wedding-suppliers/index.tsx
@@ -271,7 +271,7 @@ export default function WeddingSuppliers() {
                 />
             </div>
 
-            {/*  section */}
+            {/* table section */}
             <div className="bg-white dark:bg-gray-900 rounded-lg shadow overflow-hidden">
                 <CrudTable
                     columns={columns}
