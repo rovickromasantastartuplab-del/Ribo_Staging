809e7d79effaa47f6905156457420b7411f564b5
Rovick <rovickromasanta.startuplab@gmail.com>
2026-03-04 16:37:23 +0800

    50m

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -641,7 +641,7 @@ class LeadController extends Controller
         }
 
         $request->validate([
-            'file' => 'required|mimes:csv,txt,xls,xlsx|max:10240',
+            'file' => 'required|mimes:csv,txt,xls,xlsx|max:51200',
         ]);
 
         try {
