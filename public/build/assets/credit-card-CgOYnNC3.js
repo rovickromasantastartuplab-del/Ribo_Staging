52ec5077e548921f1a15a0e77b35ed22f977f8ad
Rovick <rovickromasanta.startuplab@gmail.com>
2026-04-06 17:21:24 +0800

    revert

--- public/build/assets/credit-card-CgOYnNC3.js
+++
@@ -1,6 +0,0 @@
-import{t as e}from"./app-0l69WN3T.js";/**
- * @license lucide-react v0.475.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const t=[["rect",{width:"20",height:"14",x:"2",y:"5",rx:"2",key:"ynyp8z"}],["line",{x1:"2",x2:"22",y1:"10",y2:"10",key:"1b3vmo"}]],o=e("CreditCard",t);export{o as C};
